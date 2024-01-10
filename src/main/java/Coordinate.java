cf19194203f24ff865be79d1a0d4ff5da204e0b9
ajorgep <alejorpas2@alu.edu.gva.es>
2024-01-10 19:27:47 +0100

    Commit

--- src/main/java/Coordinate.java
+++ src/main/java/Coordinate.java
@@ -48,10 +48,13 @@ public class Coordinate {
     public boolean equals(Object obj){
         //es nulo?
         if(obj==null) return false;
+
         //es una instancia de Coordenada?
         if(!(obj instanceof Coordinate)) return false;
+
         //casting
         Coordinate coordinate = (Coordinate) obj;
+
         //comprobación
         return (coordinate.letter==this.letter &&
                 coordinate.number==this.number);
