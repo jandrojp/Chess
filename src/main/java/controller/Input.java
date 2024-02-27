906819e83c9c767cee6eca835374e5d0109aded2
ajorgep <alejorpas2@alu.edu.gva.es>
2024-02-27 18:43:03 +0100

    Commit

--- src/main/java/controller/Input.java
+++ src/main/java/controller/Input.java
@@ -23,24 +23,6 @@ public class Input {
         return sc.nextInt();
     }
 
-
-    /**
-     *
-     * @param message Message that arrives asking for an integer
-     * @return Returns an integer
-     */
-    public static int getIntegerTwo(String message) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print(message);
-        while(!sc.hasNextInt()){
-            System.out.print("                                                                " + Console.ANSI_RED + "   ERROR, WRITE A INTEGER NUMBER ! \uD83D\uDE21 " + Console.ANSI_RESET);
-            System.out.println();
-            System.out.print(message);
-            sc.next();
-        }
-        return sc.nextInt();
-    }
-
     /**
      *
      * @return Returns a coordinate as a String
@@ -55,21 +37,4 @@ public class Input {
 
         return coordinate;
     }
-
-    /**
-     *
-     * @return Returns a coordinate as a String
-     */
-    public static String getCoordinateTwo() {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("                                                           " + Console.ANSI_BLUE + "Enter a coordinate" + Console.ANSI_RESET + " (Example 'DA' / 'da')" + Console.ANSI_BLUE + ": " + Console.ANSI_RESET);
-        String coordinate = sc.next();
-        coordinate = coordinate.toUpperCase();
-
-        return coordinate;
-    }
-
-
 }
--- src/main/java/view/Console.java
+++ src/main/java/view/Console.java
@@ -7,9 +7,8 @@ public class Console {
     public static final String GREEN_BACKGROUND = "\u001B[42m";
     public static final String GREY_BACKGROUND = "\u001B[47m";
     public static final String BLUE_BACKGROUND = "\033[44m";
-    public static final String WHITE_BACKGROUND = "\u001B[47m";
     public static final String YELLOW_BACKGROUND = "\u001B[43m";
-    public static final String BLACK_BACKGROUND = "\u001B[40m";
+
 
     // FONT COLORS
     public static final String ANSI_RESET = "\u001B[0m";
@@ -17,9 +16,7 @@ public class Console {
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_BLACK = "\u001B[30m";
 
 }
